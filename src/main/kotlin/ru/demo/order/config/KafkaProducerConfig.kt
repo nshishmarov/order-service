908a1c169dcb3494272c7588506bf5e715a9bdec
Nikita Shishmarov <nshishmarov@diasoft.ru>
2025-03-23 17:49:57 +0300

    feat: Kafka producer config

---
+++ src/main/kotlin/ru/demo/order/config/KafkaProducerConfig.kt
@@ -0,0 +1,36 @@
+package ru.demo.order.config
+
+import com.fasterxml.jackson.databind.JsonSerializer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import ru.demo.order.controller.dto.Order
+
+@Configuration
+class KafkaProducerConfig {
+
+    @Autowired
+    private lateinit var kafkaProperties: KafkaProperties
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Order> {
+        val config = kafkaProperties.buildProducerProperties()
+        config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaProperties.bootstrapServers
+        config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
+        return DefaultKafkaProducerFactory(config)
+    }
+
+    @Bean
+    private fun kafkaTemplate(
+        producerFactory: ProducerFactory<String, Order>
+    ) : KafkaTemplate<String, Order> {
+        return KafkaTemplate(producerFactory)
+    }
+}
