f6881180c48b97ef21c7827d7e8119781b9539f6
Nikita Shishmarov <nshishmarov@diasoft.ru>
2025-03-23 17:59:49 +0300

    refactoring: added general app config

--- src/main/kotlin/ru/demo/order/OrderApplication.kt
+++ src/main/kotlin/ru/demo/order/OrderApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["ru.demo.order"])
-@EnableJpaRepositories(basePackages = ["ru.demo.order.repository"])
 class OrderApplication
 
 fun main(args: Array<String>) {
