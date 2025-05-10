0649b108dae001eadb86caf07fdf61d9bb88ef81
Nikita Shishmarov <nshishmarov@diasoft.ru>
2025-05-10 19:01:35 +0300

    feat: added Streams Topology && Order Serde

--- src/main/kotlin/ru/demo/order/kafka/deserializer/PaymentDeserializer.kt
+++ src/main/kotlin/ru/demo/order/kafka/deserializer/PaymentDeserializer.kt
@@ -1,13 +1,13 @@
-//package ru.demo.order.kafka.deserializer
-//
-//import com.fasterxml.jackson.databind.ObjectMapper
-//import org.apache.kafka.common.serialization.Deserializer
-//import ru.demo.order.controller.dto.Payment
-//
-//class PaymentDeserializer : Deserializer<Payment> {
-//
-//    private val objectMapper = ObjectMapper()
-//
-//    override fun deserialize(kafkaTopic: String?, paymentByte: ByteArray?): Payment =
-//        objectMapper.readValue(paymentByte.toString(), Payment::class.java)
-//}
+package ru.demo.order.kafka.deserializer
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.kafka.common.serialization.Deserializer
+import ru.demo.order.controller.dto.Payment
+
+class PaymentDeserializer : Deserializer<Payment> {
+
+    private val objectMapper = ObjectMapper()
+
+    override fun deserialize(kafkaTopic: String?, paymentByte: ByteArray?): Payment =
+        objectMapper.readValue(paymentByte.toString(), Payment::class.java)
+}
--- src/main/kotlin/ru/demo/order/kafka/serializer/OrderSerializer.kt
+++ src/main/kotlin/ru/demo/order/kafka/serializer/OrderSerializer.kt
@@ -1,19 +1,19 @@
-//package ru.demo.order.kafka.serializer
-//
-//import com.fasterxml.jackson.databind.ObjectMapper
-//import lombok.AllArgsConstructor
-//import lombok.NoArgsConstructor
-//import org.apache.kafka.common.serialization.Serializer
-//import ru.demo.order.controller.dto.Order
-//import java.io.NotSerializableException
-//
-//@AllArgsConstructor
-//@NoArgsConstructor
-//open class OrderSerializer : Serializer<Order> {
-//
-//    private val objectMapper = ObjectMapper()
-//
-//    override fun serialize(topic: String?, order: Order?): ByteArray =
-//        objectMapper.writeValueAsBytes(order ?:
-//        throw NotSerializableException("Error with serialization!"))
-//}
+package ru.demo.order.kafka.serializer
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import lombok.AllArgsConstructor
+import lombok.NoArgsConstructor
+import org.apache.kafka.common.serialization.Serializer
+import ru.demo.order.controller.dto.Order
+import java.io.NotSerializableException
+
+@AllArgsConstructor
+@NoArgsConstructor
+open class OrderSerializer : Serializer<Order> {
+
+    private val objectMapper = ObjectMapper()
+
+    override fun serialize(topic: String?, order: Order?): ByteArray =
+        objectMapper.writeValueAsBytes(order ?:
+        throw NotSerializableException("Error with serialization!"))
+}
